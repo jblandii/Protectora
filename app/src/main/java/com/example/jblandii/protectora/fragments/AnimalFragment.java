4fca609b88a04075c509bd1eba583c1dbf27198b
Jairo Ariza Martinez <jairo_ariza_95@hotmail.com>
2018-04-26 19:12:00 +0200

    Modificacion de los botones del los animales y protectoras.

--- app/src/main/java/com/example/jblandii/protectora/fragments/AnimalFragment.java
+++ app/src/main/java/com/example/jblandii/protectora/fragments/AnimalFragment.java
@@ -1,15 +1,22 @@
 package com.example.jblandii.protectora.fragments;
 
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.jblandii.protectora.LoginActivity;
+import com.example.jblandii.protectora.Models.Animal;
 import com.example.jblandii.protectora.R;
+import com.example.jblandii.protectora.RecordarContrasenaActivity;
 
 public class AnimalFragment extends Fragment {
+
+    FloatingActionButton fab_filtrar_protectora;
 
     public AnimalFragment() {
     }
@@ -20,8 +27,23 @@ public class AnimalFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_animal_fragment, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_animal_fragment, container, false);
+        fab_filtrar_protectora = view.findViewById(R.id.fab);
+        fab_filtrar_protectora.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent intentRecordarContrasena = new Intent(getActivity(), RecordarContrasenaActivity.class);
+//                startActivity(intentRecordarContrasena);
+            }
+        });
+        return view;
+    }
+
+    /**
+     * Metodo que permite cargar los animales que pertenecen a la misma comunidad que el usuario.
+     */
+    public void cargarAnimales() {
+
     }
 }
